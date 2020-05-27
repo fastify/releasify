d195dff3920220a06165735414f09432c4002a66
Manuel Spigolon <behemoth89@gmail.com>
2020-05-27 22:57:51 +0200

    add version flag

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,5 +1,32 @@
 'use strict'
 
 const t = require('tap')
+const h = require('./helper')
+const pkg = require('../package.json')
 
-t.skip('cli test must be implemented')
+t.test(`version`, t => {
+  t.plan(1)
+  const cli = h.execute('', ['--version'])
+  cli.stdout.setEncoding('utf8')
+  cli.stdout.on('data', output => {
+    t.like(output, pkg.version)
+  })
+})
+
+t.test(`version strict`, t => {
+  t.plan(1)
+  const cli = h.execute('', ['-v'])
+  cli.stdout.setEncoding('utf8')
+  cli.stdout.on('data', output => {
+    t.like(output, pkg.version)
+  })
+})
+
+t.test(`version win over help`, t => {
+  t.plan(1)
+  const cli = h.execute('', ['-h', '-v'])
+  cli.stdout.setEncoding('utf8')
+  cli.stdout.on('data', output => {
+    t.like(output, pkg.version)
+  })
+})
