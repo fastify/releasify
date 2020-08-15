ec2c607f60a00230abfaa75f5f44d572f98f75f1
Manuel Spigolon <behemoth89@gmail.com>
2020-08-15 09:58:59 +0200

    add test script to check action

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh -l
 
-echo "Hello $*"
-time=$(date)
-#echo "::set-output name=time::$time"
+# echo params for debug during impl
+echo "Params $*"
+
+# draft for test
+releasify draft \
+  --semver=${1} \
+  --remote=${2} \
+  --branch=${3} \
+  --npm-otp=${4} \
+  --npm-dist-tag=${5} \
+  --major=${6}
