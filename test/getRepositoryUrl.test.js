94cbfd361c5472f2dac6d8a3021519c0054f3cd8
Jiří Špác <capajj@gmail.com>
2021-09-24 18:16:22 +0200

    fix: support both types of "repository" in package.json

---
+++ test/getRepositoryUrl.test.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const { test } = require('tap')
+const getRepositoryUrl = require('../lib/commands/getRepositoryUrl')
+
+test('getRepositoryUrl', async (t) => {
+  const url = getRepositoryUrl({
+    getRepoPackage: () => ({
+      repository: 'git+https://github.com/foo/bar.git'
+    })
+  })
+
+  t.equal(url, 'git+https://github.com/foo/bar.git')
+
+  const url2 = getRepositoryUrl({
+    getRepoPackage: () => ({
+      repository: {
+        type: 'git',
+        url: 'git+https://github.com/foo/bar.git'
+      }
+    })
+  })
+
+  t.equal(url2, 'git+https://github.com/foo/bar.git')
+})
