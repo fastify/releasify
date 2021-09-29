80cd6493c805cb0de2d00aa60c6f42e39f788286
Jiří Špác <capajj@gmail.com>
2021-09-29 10:53:30 +0200

    test: add missing spec for the throw

--- test/getRepositoryUrl.test.js
+++ test/getRepositoryUrl.test.js
@@ -3,23 +3,39 @@
 const { test } = require('tap')
 const getRepositoryUrl = require('../lib/getRepositoryUrl')
 
-test('getRepositoryUrl', async (t) => {
-  const url = getRepositoryUrl({
-    getRepoPackage: () => ({
-      repository: 'git+https://github.com/foo/bar.git'
+test('getRepositoryUrl', (t) => {
+
+  test('returns url', (t) => {
+    const url = getRepositoryUrl({
+      getRepoPackage: () => ({
+        repository: 'git+https://github.com/foo/bar.git'
+      })
     })
+
+    t.equal(url, 'git+https://github.com/foo/bar.git')
+
+    const url2 = getRepositoryUrl({
+      getRepoPackage: () => ({
+        repository: {
+          type: 'git',
+          url: 'git+https://github.com/foo/bar.git'
+        }
+      })
+    })
+
+    t.equal(url2, 'git+https://github.com/foo/bar.git')
+    t.end()
   })
 
-  t.equal(url, 'git+https://github.com/foo/bar.git')
-
-  const url2 = getRepositoryUrl({
-    getRepoPackage: () => ({
-      repository: {
-        type: 'git',
-        url: 'git+https://github.com/foo/bar.git'
-      }
+  test('throws when repository is missing', (t) => {
+    t.throws(() => {
+      getRepositoryUrl({
+        getRepoPackage: () => ({})
+      })
     })
-  })
+    t.end()
 
-  t.equal(url2, 'git+https://github.com/foo/bar.git')
+  })
+  t.end()
+
 })
